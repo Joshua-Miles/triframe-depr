b7fb59b36d9de47cf87dbccbc574d6fea4ba3c39
Joshua Miles <isaacsmiles@gmail.com>
2019-08-29 08:40:29 -0500

    Finished composition decorator

--- arbiter/index.js
+++ arbiter/index.js
@@ -1,6 +1,6 @@
-import {  _shared, _stream, _authorize, _public, _session, _validate, _compose } from "./Markers"
+import {  _shared, _stream, _authorize, _public, _session, _validate, _composes } from "./Markers"
 import { Serializer } from "./Serializer";
 import { UnSerializer } from "./UnSerializer";
 export {
-    Serializer, UnSerializer, _shared, _stream, _authorize, _public, _session, _validate, _compose
+    Serializer, UnSerializer, _shared, _stream, _authorize, _public, _session, _validate, _composes
 }
